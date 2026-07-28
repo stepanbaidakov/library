909ad4d5a85fdb4c5c897f8eeeb3c7f5f53ac67c
Stepan Baidakov <stepanbaidakov@icloud.com>
2026-07-28 17:18:08 +0200

    compose.yml corrected

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ["web", "localhost", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
-    "users",
+     "users.apps.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
